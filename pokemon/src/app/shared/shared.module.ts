a372d718df3249389b15261efef40ceb759b557c
ehurafa <rafarfl2010@gmail.com>
2023-04-03 23:35:40 -0400

    create shared components

---
+++ pokemon/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { InfosComponent } from './infos/infos.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    InfosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
